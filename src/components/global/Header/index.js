7aa1b22a69949317c7bd880eb7cd045b2e0d1f71
babu tech <babutech@babus-MacBook-Air.local>
2023-07-25 17:00:37 +0530

    scss support added

--- src/components/global/Header/index.js
+++ src/components/global/Header/index.js
@@ -1,5 +1,22 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '@/styles/home.module.scss'
+
 export default function Header() {
     return (
-      <div> NavBar </div>
+      <div className={`${styles.primary_bg} ${styles.navbar_main_hold}`}>
+      <Image
+            className={`${styles.nav_logo} ${styles.navbar_main_hold}`}
+            src="/share-oh.svg"
+            alt="Share OH Logo"
+            width={140}
+            height={60}
+            priority
+          />
+          <div className={styles.nav_links_holder}>
+            <Link href="#">About</Link>
+            <Link href="#">Contact</Link>
+          </div> 
+      </div>
     )
   }
--- src/pages/frontpage.js
+++ src/pages/frontpage.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
 
 export default function Frontpage() {
   return (
@@ -8,22 +7,10 @@ export default function Frontpage() {
       <Head>
         <title>Share OH! | Frontpage</title>
         <meta name="description" content="Share and Use sources effortlessly with our share oh app" />
-      </Head>
-      <main className={`${styles.main}`}>
-        <div className={styles.description}>
-          <p>Voila! Check the products</p>
-          <div className={styles.center}>
-            <Image
-              className={styles.logo}
-              src="/share-oh.svg"
-              alt="Share OH Logo"
-              width={140}
-              height={140}
-              priority
-            />
-          </div>
-        </div>
-      </main>
+      </Head> 
+      <div>
+        <p>Voila! Front Page</p>
+      </div>
     </>
   );
 }
