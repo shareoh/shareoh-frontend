e0baf6b06ea85032c5763368361a1898c1dc6a7b
babu tech <babutech@babus-MacBook-Air.local>
2023-07-25 13:57:20 +0530

    sample pages added and vercel deploy issues fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import Header from "../components/global/Header";
+import Footer from "../components/global/Footer";
+import { ConfigProvider } from 'antd';
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider
+      theme={{
+      token: {
+        colorPrimary: '#15762e',
+      },
+    }}>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </ConfigProvider>
+  );
 }
